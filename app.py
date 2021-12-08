6ff86e1c4b235b3ff115aaa33f0503cf4306c487
qiqinn <qin@webmail.umm.ac.id>
2021-12-09 02:04:35 +0700

    fix bug

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ def predict_label(img_path):
     q.append(query)
     q = np.array(q, dtype='float') / 255.0
     q_pred = model.predict(q)
-    temp = np.round(q_pred, 1)
-    predicted_bit = int(temp)
+    if q_pred <= 0.5:
+        predicted_bit = 0
+    else:
+        predicted_bit = 1
     print(predicted_bit)
     return class_dict[predicted_bit]
 
